1752de34bb8f1b3915c8d74313ba46179751a622
Ighor Drummond <ighordrummond2001@gmail.com>
2022-01-22 16:01:01 -0300

    add fluig and javascript

--- Formulario/jsformulario.js
+++ Formulario/jsformulario.js
@@ -1,8 +1,24 @@
 
-function total(){
-	var valor 1 = #valor;
-	var valor  = #valor2;
-	var total = valor 1 + valor;
+	function total(){
+		var valorUn;
+		var quantidade;
+		var quantidade;
+		
+		valorUn = document.getElementById("vlunitario1").value
 
-	document.getElementById("ponto").innerHTML = total;
-}	
+		quantidade = document.getElementById("quantidade").value
+		for (var i=0; ){
+			var resultado = valorUn * quantidade;
+			document.getElementById("total").value = resultado;
+			}	
+	}
+	function adicionar(){
+		var camposcategaria = document.getElementsByClassName("teste");
+
+		console.log(camposcategaria);
+
+		for(var i=0; i < camposCategoria.length; i++){
+    		console.log(i)
+    		console.log(camposcategoria[i].value)
+		};
+	}	
