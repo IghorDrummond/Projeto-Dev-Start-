fa82724c5a3f246f5b245d5bfcca56549aed3aca
Ighor Drummond <ighordrummond2001@gmail.com>
2022-01-22 21:17:15 -0300

    formulario atualizado

--- Formulario/jsformulario.js
+++ Formulario/jsformulario.js
@@ -1,24 +1,9 @@
+	function incluir(){
+
+
+	}
 
 	function total(){
-		var valorUn;
-		var quantidade;
-		var quantidade;
 		
-		valorUn = document.getElementById("vlunitario1").value
 
-		quantidade = document.getElementById("quantidade").value
-		for (var i=0; ){
-			var resultado = valorUn * quantidade;
-			document.getElementById("total").value = resultado;
-			}	
 	}
-	function adicionar(){
-		var camposcategaria = document.getElementsByClassName("teste");
-
-		console.log(camposcategaria);
-
-		for(var i=0; i < camposCategoria.length; i++){
-    		console.log(i)
-    		console.log(camposcategoria[i].value)
-		};
-	}	
