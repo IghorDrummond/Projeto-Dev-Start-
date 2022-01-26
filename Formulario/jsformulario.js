0a67071f1470ed60797e3a1a86018b5ea69b2770
Ighor Drummond <ighordrummond2001@gmail.com>
2022-01-26 16:07:56 -0300

    add new graph in home.html

--- Formulario/jsformulario.js
+++ Formulario/jsformulario.js
@@ -1,5 +1,5 @@
 	function incluir(){
-
+			
 
 	}
 
@@ -7,3 +7,20 @@
 		
 
 	}
+	$("#cep").blur(function() {
+		$.getJSON("//viacep.com.br/ws/"+ $("#cep").val()+"/json/", function(dado){
+			$("#bairro").val(dado.bairro); 
+			$("#cidade").val(dado.logradouro); 
+			$("#estado").val(dado.localidade); 
+		})
+	});
+
+	$("#cep2").blur(function() {
+		$.getJSON("//viacep.com.br/ws/"+ $("#cep2").val()+"/json/", function(dado){
+			$("#bairro2").val(dado.bairro); 
+			 
+			$("#cidade2").val(dado.logradouro); 
+			$("#estado2").val(dado.localidade); 
+		})
+	});
+	
